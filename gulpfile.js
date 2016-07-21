5044db0c26782abe7e8a4998024ab8c395d19220
Luiz Medeiros <luizangelomedeiros@gmail.com>
2016-07-20 23:03:35 -0300

    Adicionado Praia no Final de semana

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,7 @@
 var gulp = require('gulp');
 var	sass = require('gulp-sass');
 var	rename = require("gulp-rename");
-/*var spritesmith = require('gulp.spritesmith');
-*/
+
 /* CONVERTE SASS PARA CSS MINIFICADO */
 gulp.task('sass', function(){
   return gulp.src('css/sass/previsaoTempo.scss')  	
@@ -16,13 +15,3 @@ gulp.task('sass', function(){
 gulp.task('watch',function() {
     gulp.watch('css/sass/previsaoTempo.scss',['sass']);
 })
-
-/*gulp.task('sprite', function () {
-    var spriteData = gulp.src('sprite/*.png')
-        .pipe(spritesmith({
-            imgName: 'arquivos/sprite.png',
-            cssName: 'css/sprite.css'
-        }));
-    spriteData.img.pipe(gulp.dest('img'));
-    spriteData.css.pipe(gulp.dest('css'));
-});*/
